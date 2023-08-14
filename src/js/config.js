087b0cccaaf9a469365b5d702d8535db1c332916
Harshal Rane <harshalr@MITRMUM.COM>
2023-08-14 14:12:34 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fbfbf6e5-529b-4e1c-b8c7-133cfad2cde7';
+export const MODAL_CLOSE_SEC = 2.5;
